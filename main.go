e3c0d52b7fc42d0f6b5759cff2b1442f910588b6
SpyrosGeo <xarodasimba@gmail.com>
2022-01-19 02:03:01 +0200

    added function for fetching linus quotes only//TODO//learn how to unmarshal json data on array

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 )
@@ -13,32 +13,52 @@ type Quote struct {
 	En     string `json:"en"`
 }
 
-func main(){
+func main() {
 	//get quote from api
-	res,err :=http.Get("https://programming-quotes-api.herokuapp.com/quotes/random")
-	if err != nil{
-		fmt.Println("No repsonse from request")	
+	res, err := http.Get("https://programming-quotes-api.herokuapp.com/quotes/random")
+	if err != nil {
+		fmt.Println("No repsonse from request")
 	}
 	defer res.Body.Close()
 	//assign response Body to variable body
-	body,err := ioutil.ReadAll(res.Body)//response Body
+	body, err := ioutil.ReadAll(res.Body) //response Body
 	//fmt.Println(string(body))
 	// after creating the struct to represent the json data
 	// create a varablie of type of the struct in this case Quote
 	var result Quote
 	//unmarshall aka convert json string to object(type)
-	if err := json.Unmarshal(body,&result); err!=nil{
+	if err := json.Unmarshal(body, &result); err != nil {
 		fmt.Println("Cannot unmarshal JSON")
 	}
 
 	// fmt.Println(PrettyPrint(result))
 	// print the quote part from the struct
-	fmt.Printf("%s-%s",result.En,result.Author)
-
-
+	fmt.Printf("%s-%s", result.En, result.Author)
+	// getLinusQuotes()
 }
+
 //this is used to make the print pretty for the full json
-func PrettyPrint(i interface {})string {
-	s,_:=json.MarshalIndent(i,"","\t")
-	return  string(s)
+func PrettyPrint(i interface{}) string {
+	s, _ := json.MarshalIndent(i, "", "\t")
+	return string(s)
+}
+
+func getLinusQuotes() {
+	//get all linus quotes
+	res, err := http.Get("https://programming-quotes-api.herokuapp.com/quotes/author/Linus%20Torvalds")
+	if err != nil {
+		fmt.Println("No repsonse from request")
+	}
+	defer res.Body.Close()
+	//assign response Body to variable body
+	body, err := ioutil.ReadAll(res.Body) //response Body
+	//fmt.Println(string(body))
+	// after creating the struct to represent the json data
+	// create a varablie of type of the struct in this case Quote
+	var result Quote
+	//unmarshall aka convert json string to object(type)
+	if err := json.Unmarshal(body, &result); err != nil {
+		fmt.Println("Cannot unmarshal JSON")
+	}
+	fmt.Println(body)
 }
